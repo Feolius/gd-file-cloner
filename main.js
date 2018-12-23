1a294456e05103d323ac30e459d03de385fbd1b1
Feolius <feolius@gmail.com>
2018-12-24 00:01:40 +0700

    Added search by owner functionality and file grid

--- main.js
+++ main.js
@@ -1,136 +1,91 @@
-// Client ID and API key from the Developer Console
-var CLIENT_ID = '566991952673-6ricdqhl0ugoskgvqe5lnf4fvokj8bsl.apps.googleusercontent.com';
-var API_KEY = 'AIzaSyBodPny5DnvsbQh2Md8Aou2a0hxmy9J9O4';
-
-// Array of API discovery doc URLs for APIs used by the quickstart
-var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
-
-// Authorization scopes required by the API; multiple scopes can be
-// included, separated by spaces.
-var SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly';
-
-var authorizeButton = document.getElementById('authorize_button');
-var signoutButton = document.getElementById('signout_button');
-
-/**
- *  On load, called to load the auth2 library and API client library.
- */
-function handleClientLoad() {
-    gapi.load('client:auth2', initClient);
-}
-
-/**
- *  Initializes the API client library and sets up sign-in state
- *  listeners.
- */
-function initClient() {
-    gapi.client.init({
-        apiKey: API_KEY,
-        clientId: CLIENT_ID,
-        discoveryDocs: DISCOVERY_DOCS,
-        scope: SCOPES
-    }).then(function () {
-        // Listen for sign-in state changes.
-        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-
-        // Handle the initial sign-in state.
-        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-        authorizeButton.onclick = handleAuthClick;
-        signoutButton.onclick = handleSignoutClick;
-    }, function(error) {
-        appendPre(JSON.stringify(error, null, 2));
-    });
-}
-
-/**
- *  Called when the signed in status changes, to update the UI
- *  appropriately. After a sign-in, the API is called.
- */
-function updateSigninStatus(isSignedIn) {
-    if (isSignedIn) {
-        authorizeButton.style.display = 'none';
-        signoutButton.style.display = 'block';
-        listFiles();
-    } else {
-        authorizeButton.style.display = 'block';
-        signoutButton.style.display = 'none';
-    }
-}
-
-/**
- *  Sign in the user upon button click.
- */
-function handleAuthClick(event) {
-    gapi.auth2.getAuthInstance().signIn();
-}
-
-/**
- *  Sign out the user upon button click.
- */
-function handleSignoutClick(event) {
-    gapi.auth2.getAuthInstance().signOut();
-}
-
-/**
- * Append a pre element to the body containing the given message
- * as its text node. Used to display the results of the API call.
- *
- * @param {string} message Text to be placed in pre element.
- */
-function appendPre(message) {
-    var pre = document.getElementById('content');
-    var textContent = document.createTextNode(message + '\n');
-    pre.appendChild(textContent);
-}
-
-/**
- * Print files.
- */
-function listFiles() {
-    gapi.client.drive.files.list({
-        'pageSize': 10,
-        'fields': "nextPageToken, files(id, name)"
-    }).then(function(response) {
-        appendPre('Files:');
-        var files = response.result.files;
-        if (files && files.length > 0) {
-            for (var i = 0; i < files.length; i++) {
-                var file = files[i];
-                appendPre(file.name + ' (' + file.id + ')');
-            }
-        } else {
-            appendPre('No files found.');
-        }
-    });
-}
-
-document.addEventListener('DOMContentLoaded', function(){
-    chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
+const ENTER_KEY_CODE = 13;
+$(() => {
+    'use strict';
+    chrome.identity.getAuthToken({'interactive': true}, (token) => {
         gapi.load('client', {
-            callback: function() {
-                // Handle gapi.client initialization.
+            callback: () => {
                 gapi.client.setToken({access_token: token});
-                var request = gapi.client.request({
-                    'method': 'GET',
-                    'path': 'https://www.googleapis.com/drive/v3/files.list',
-                    'params': {'fields': 'file'}
-                });
-                request.execute(function(response) {
-                    console.log(response);
-                });
             },
-            onerror: function() {
-                // Handle loading error.
+            onerror: () => {
+                // @TODO handle error here properly
                 alert('gapi.client failed to load!');
             },
             timeout: 5000, // 5 seconds.
-            ontimeout: function() {
-                // Handle timeout.
+            ontimeout: () => {
+                // @TODO handle error here properly
                 alert('gapi.client could not load in a timely manner!');
             }
         });
-        console.log(token);
 
+        function gapiClientLoadCb() {
+            // Handle gapi.client initialization.
+
+            var request = gapi.client.request({
+                'method': 'GET',
+                'path': "/drive/v3/files?",
+                'params': {
+                    'fields': 'nextPageToken, files(id, name, parents, iconLink, owners)',
+                    'q': '"ivan@zgtec.com" in owners'
+                }
+            });
+            request.execute(function (response) {
+                console.log(response);
+            });
+        }
+
+        const filesContainer = $('.files-container');
+
+        function searchHandler(searchStr) {
+            var request = gapi.client.request({
+                'method': 'GET',
+                'path': "/drive/v3/files?",
+                'params': {
+                    'fields': 'nextPageToken, files(id, name, parents, iconLink, owners, modifiedTime, size)',
+                    'q': `"${searchStr}" in owners and mimeType != "application/vnd.google-apps.folder"`
+                }
+            });
+            request.execute(function (response) {
+                console.log(response);
+                if (response.files !== undefined) {
+                    if (response.files.length > 0) {
+                        buildFilesGrid(response.files);
+                        $('#select-all').prop("disabled", false);
+                    } else {
+                        $('#select-all').prop("disabled", true);
+                        filesContainer.html('No files available');
+                    }
+                } else {
+                    // @TODO handle error here
+                }
+            });
+        }
+
+        $('#search-box input').keypress(function (event) {
+            if (event.which === ENTER_KEY_CODE) {
+                searchHandler($(this).val())
+            }
+        });
+
+        $('#search-box .btn').click(() => {
+            searchHandler($('#search-box input').val())
+        });
+
+        function buildFilesGrid(files) {
+            filesContainer.empty();
+            for (let file of files) {
+                let filesRow = $(`<div id="${file.id}" class="row file-row"></div>`);
+                filesRow.append(`<div class="col-xs-1"><img src="${file.iconLink}"></div>`);
+                filesRow.append(`<div class="col-xs-5">${file.name}</div>`);
+                filesRow.append(`<div class="col-xs-3">${file.owners[0].displayName}</div>`);
+                filesRow.append(`<div class="col-xs-3">${file.modifiedTime}</div>`);
+                filesContainer.append(filesRow);
+            }
+            $('.file-row').click(fileRowClickHandler)
+        }
+
+        function fileRowClickHandler(event) {
+            $(this).toggleClass('checked');
+        }
     });
-    // listFiles();
 });
+
